e05f65ceb164421e815071d736a108d091b84a4c
Abhishek Tiwari <69257109+Itz-Abhishek-Tiwari@users.noreply.github.com>
2024-04-24 20:53:16 +0530

    added the config files of nvchad

---
+++ init.lua
@@ -0,0 +1,48 @@
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require("configs.lazy")
+
+-- load plugins
+require("lazy").setup({
+	{
+		"NvChad/NvChad",
+		lazy = false,
+		branch = "v2.5",
+		import = "nvchad.plugins",
+		config = function()
+			require("options")
+		end,
+	},
+
+	{ import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require("nvchad.autocmds")
+
+vim.schedule(function()
+	require("mappings")
+end)
+
+-- user added
+vim.opt.swapfile = false
+vim.opt.colorcolumn = "80"
+vim.opt.mouse = ""
+
+-- custom key to move line
+vim.api.nvim_set_keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
