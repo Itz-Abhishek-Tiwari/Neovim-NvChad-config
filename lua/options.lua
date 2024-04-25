6693487fb905fc3901f62812145ab35dcc6e1d4b
Abhishek Tiwari <69257109+Itz-Abhishek-Tiwari@users.noreply.github.com>
2024-04-25 13:06:30 +0530

    add markdown Support for my nvim config

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,20 @@
-require "nvchad.options"
+require("nvchad.options")
 
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
+
+-- peek.nvim setup
+
+require("peek").setup({
+	auto_load = true,
+	close_on_bdelete = true,
+	syntax = true,
+	theme = "light",
+	update_on_change = true,
+	app = "webview",
+	filetype = { "markdown" },
+	throttle_at = 200000,
+	throttle_time = "auto",
+})
